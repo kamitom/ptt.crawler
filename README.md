3d89fe91f69daf966e284dc0fbacd9f7390f92f2
Tom Chen <tom@itri.org.tw>
2019-12-08 14:03:51 +0800

    Add README content

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 練習 python
+
+## 參考朋朋老師的教學
+
+[This link](https://www.youtube.com/watch?v=BEA7F9ExiPY&list=PL-g0fdC5RMboYEyt6QS2iLb_1m7QcgfHk&index=20)
--- crawler.py
+++ crawler.py
@@ -9,8 +9,8 @@ import urllib.request as req
 # 處理: urllib.error.URLError: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed (_ssl.c:852)>
 ssl._create_default_https_context = ssl._create_unverified_context
 
-pttNBAurl = "https://www.ptt.cc/bbs/NBA/index6495.html"
-# pttNBAurl = "https://www.ptt.cc/bbs/NBA/index.html"
+# pttNBAurl = "https://www.ptt.cc/bbs/NBA/index6495.html"
+pttNBAurl = "https://www.ptt.cc/bbs/NBA/index.html"
 # 建立一個request 物件,附加 headers 資訊
 request = req.Request(pttNBAurl, headers={
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
